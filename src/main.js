4128e627d01bb1ddc346b13e323f075e79fc76e1
MingLiCheng <43997330+MingLiCheng@users.noreply.github.com>
2018-11-23 19:39:58 +0800

    header+tabbar

--- src/main.js
+++ src/main.js
@@ -1,11 +1,21 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+
+// 引入Mint-UI
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+
+// 引入mui
+import './assets/mui/css/mui.css'
+
 import App from './App'
 import router from './router'
 
-Vue.config.productionTip = false
 
+
+Vue.config.productionTip = false
+Vue.use(MintUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
